e420dd2213369061851877e16ab4dff5f65567a7
Julieta-ph <julietapinedo2828@gmail.com>
2024-06-12 00:14:21 -0300

    Cambios Client Controller Task9

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -270,7 +270,59 @@ namespace HomeBanking2._0.Controllers
         }
 
 
+        // traer el cliente autenticado, confirmar que tiene menos de 6 tarjetas.
+        // Si esta OK, crear tarjeta y asignarla. Los numeros de tarjeta y cvv deben ser aleatorios
 
+        [HttpPost("current/cards")]
+        [Authorize(Policy = "ClientOnly")]
+
+        public IActionResult CreateCardToClientAuthenticated([FromBody] NewCardDTO newCardDTO)
+        {
+            try
+            {
+                Client clientCurrent = GetCurrentClient();
+
+                var cardByClient = _cardService.GetAllCardsByType(clientCurrent.Id, newCardDTO.type);
+
+                if (cardByClient.Count() < 3)
+                {
+                    if (!cardByClient.Any(c => c.Color == newCardDTO.color))
+                    {
+                        var newCard = new Card
+                        {
+                            ClientId = clientCurrent.Id,
+                            CardHolder = clientCurrent.FirstName + " " + clientCurrent.LastName,
+                            Color = newCardDTO.color,
+                            Cvv = _cardService.GenerateCVV(),
+                            FromDate = DateTime.Now,
+                            ThruDate = DateTime.Now.AddYears(5),
+                            Number = _cardService.GenerateNumberCard(clientCurrent.Id),
+                            Type = newCardDTO.type,
+                        };
+
+                        _cardService.AddCard(newCard);
+
+
+                    }
+                    else
+                    {
+                        return StatusCode(403, "No es posible crear otra tarjeta, llegaste al límite disponible");
+                    }
+
+
+
+                }
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+
+
+        }
 
 
 
